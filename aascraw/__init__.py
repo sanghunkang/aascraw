b8067d11ef0c4bb2bd409025dabb794b8bfa5ad4
sanghunkang <sanghunkang.dev@gmail.com>
2019-08-08 00:14:44 +0900

    clarified roles of each method

--- aascraw/__init__.py
+++ aascraw/__init__.py
@@ -5,16 +5,15 @@ class Agent():
             # action_key: [action_value, action_rank]
         }
 
-    # def __search_action_space(self):
-    #     raise NotImplementedError
+    def update_action_space(self):
+        raise NotImplementedError
 
     def update_policy(self, rank_deltas):
-        self.__search_action_space()
         for rank_delta in rank_deltas:
             action_key = rank_delta[0]
             action_value = rank_delta[1]
             action_rank = rank_delta[2]
-            if action_key in actions:
+            if action_key in self.actions:
                 self.actions[action_key][1] += action_rank
             else:
                 self.actions[action_key] = [action_value, action_rank]
@@ -23,7 +22,10 @@ class Master(Agent):
     def __init__(self):
         super().__init__()
 
-    def __search_action_space(self):
+    def proceed(self):
+        pass
+
+    def update_action_space(self):
         print("This function will look for possible event triggers and get queries")
 
     def give_task(self):
@@ -36,17 +38,39 @@ class Slave(Agent):
     def __init__(self):
         super().__init__()
 
-    def __search_action_space(self):
+    def load_task(self, task):
+        pass
+
+    def update_action_space(self):
         print("This function will look for selector sequences")
 
-    def run_task(self, task):
+    def run_task(self):
         # Locate element
 
         return
 
 class Storage():
     def __init__(self):
-        pass     
+        self.task_log = [
+            # action_master, action_slave, consistent_features, variant_features, rank_delta
+        ] 
 
-    def get_rank_delta(self):
+    def ingest(self, result):
+        # This function saves newly collected result
+        pass
+
+    def evaluate_data(self):
+        # This function evaluates objective functions
+        
+        # calculate_contrib_to_consistency
+        # caclulate_contrib_to_variety
+        pass
+
+    def get_rank_delta_master(self):
+        # [action_master, rank_delta]
+        pass
+
+    def get_rank_delta_slave(self):
+        # [action_key, rank_delta]
+
         pass
--- example.py
+++ example.py
@@ -1,18 +1,34 @@
 from aascraw import Master, Slave, Storage
 
-
-
 master = Master() 
 slave = Slave()
 storage = Storage()
 
 rank_delta = None
+
+# Exploration step
 for i in range(100):
-    master.update_policy(rank_delta) #
-    slave.update_policy(rank_delta) #
-
+    master.proceed()
+    master.update_action_space() #
+    master.update_policy(rank_delta_slave) #
     task = master.give_task()
-    data = slave.run_task(task)
+    
+    slave.load_task(task)
+    slave.update_action_space() #
+    slave.update_policy(rank_delta) #
+    data = slave.run_task()
 
-    storage.append(data)
-    rank_delta = storage.get_rank_delta()
+    storage.ingest(data)
+    storage.evaluate_data()
+    rank_delta_master = storage.get_rank_delta_master()
+    rank_delta_slave = storage.get_rank_delta_slave()
+    
+# Exploitation step
+for i in range(100):
+    master.proceed()            # move on to the next page
+    task = master.give_task()   
+    
+    slave.load_task(task)
+    data = slave.run_task()     # locate elements which contain desired information 
+
+    storage.ingest(data)        # save the data at located elements
