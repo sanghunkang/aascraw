1ca21951bd72d630b889035113982001e0d5d25d
sanghunkang <sanghunkang.dev@gmail.com>
2019-08-28 23:52:52 +0900

    cval

--- aascraw/c_kernels.c
+++ aascraw/c_kernels.c
@@ -1,56 +1,74 @@
+#include <stdio.h>
+#include <signal.h>
 // #include "iostream"
 // #include <vector> 
 // using namespace std; 
 
-// float rank_tuple_vicinity(char xpath_set[4][500])
 
-float rank_tuple_vicinity(char ** xpath_set)
+// float rank_tuple_vicinity(char **xpath_set)
+int rank_tuple_vicinity(char **xpath_set)
 {
     // DO I REALLY NEED VECTOR, OR IS IT OKAY WITH STRING ARRAYS?
-    // int pos = 0;
-    // int numer = 0;
-    // int denom = 0;
-    printf("%c\n", xpath_set[0][0]);
+    int pos = 0;
+    int numer = 0;
+    int denom = 0;
+    
+    static char *xxpath_set= { "A", "B", "C", NULL };
+
+    int len_xpathSet = 0;
+    while (xxpath_set[len_xpathSet] != NULL)
+    {
+        len_xpathSet++;
+    }
+
+    // while (xpath_set[len_xpathSet] != NULL)
+    // {
+    //     len_xpathSet++;
+    // }
+
+    return len_xpathSet;
     // while (pos < xpath_set[0].size()) 
+    // while (pos < sizeof(xpath_set[0])/sizeof(char))
     // {
 
-    //     // Filling in a vertical slice
-    //     int vertical_slice[xpath_set.size()] = {};
-    //     for (int i=0; i++; i < xpath_set.size()) 
-    //     {
-    //         if (pos < xpath_set[i][0].size())
-    //         {
-    //             vertical_slice[i] = xpath_set[i][pos];
-    //         }
+        // Filling in a vertical slice
+        // int vertical_slice[sizeof(xpath_set[0])] = {};
+        // for (int i=0; i < sizeof(xpath_set[0]); i++) 
+        // {
+        //     if (pos < sizeof(xpath_set[i]))
+        //     {
+        //         vertical_slice[i] = xpath_set[i][pos];
+        //     }
             
-    //     }
+        // }
 
-    //     // Calculate the numerator value
-    //     int matching_score = 1;
-    //     for (int i=0; i++; i < xpath_set.size())
-    //     {
-    //         int temp_matching_score = 1;
-    //         char c = vertical_slice[i]
-    //         for (int j=0; j++; j < xpath_set.size())
-    //         {
-    //             if (c == vertical_slice[j])
-    //             {
-    //                 temp_matching_score++;
-    //             }
-    //         }
+        // Calculate the numerator value
+        // int matching_score = 1;
+        // for (int i=0; i < sizeof(xpath_set[0]); i++)
+        // {
+        //     int temp_matching_score = 1;
+        //     char c = vertical_slice[i];
+        //     for (int j=0; j < sizeof(xpath_set[0]); j++)
+        //     {
+        //         if (c == vertical_slice[j])
+        //         {
+        //             temp_matching_score++;
+        //         }
+        //     }
             
-    //         if (matching_score < temp_matchin_score)
-    //         {
-    //             matching_score = temp_matching_score;
-    //         }
-    //     }
+        //     if (matching_score < temp_matching_score)
+        //     {
+        //         matching_score = temp_matching_score;
+        //     }
+        // }
 
-    //     //     print(vertical_slice)
-    //     numer += matching_score;
-    //     denom += xpath_set[0].size(); // MAYBE ADD ONLY WHEN TRUE
+        //     print(vertical_slice)
+        // numer += matching_score;
+        // numer += 1;
+        // denom += sizeof(xpath_set)/sizeof(char*); // MAYBE ADD ONLY WHEN TRUE
         
-    //     pos ++;
+        // pos ++;
     // }        
     // return (float)numer/(float)denom; // TYPE CONVERSION!
-    return 1.0;
+    // return 1.0;
 }
--- aascraw/kernels.py
+++ aascraw/kernels.py
@@ -21,23 +21,27 @@ c_kernels = ctypes.CDLL("./aascraw/c_kernels.so")
 # c_kernels.rank_tuple_vicinity.argtypes = (ctypes.c_float*4, )
 
 c_kernels.rank_tuple_vicinity.argtypes = (ctypes.c_wchar_p * SIZE_XPATH_SET, )
-c_kernels.rank_tuple_vicinity.restype = ctypes.c_float
+# c_kernels.rank_tuple_vicinity.restype = ctypes.c_float
+c_kernels.rank_tuple_vicinity.restype = ctypes.c_int
 
 arr = ctypes.c_wchar_p * SIZE_XPATH_SET
 parameter_array = arr(*["array", "of", "strings", "asdasd"])
 
 
-print(c_kernels.rank_tuple_vicinity(parameter_array))
+# print(c_kernels.rank_tuple_vicinity(parameter_array))
 # (xpath_set, existing_records)
 # rank_tuple_vicinity = c_kernels.rank_tuple_vicinity  
 def rank_tuple_vicinity(xpath_set, existing_records):
 
     # actions = xpath_set[i]["filterer_action"]
     filterer_actions = [xpath["filterer_action"] for xpath in xpath_set]
-    print(filterer_actions)
+    # print(filterer_actions)
     c_xpath_set = (ctypes.c_wchar_p * SIZE_XPATH_SET)(*filterer_actions)
 
+    print(len(filterer_actions))
+    print(c_xpath_set)
     rank = c_kernels.rank_tuple_vicinity(c_xpath_set)
+    print(rank)
     return rank
 
 
