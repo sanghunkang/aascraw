341b51f106e6d2e891d5c9a8917df7cf5f979c3d
sanghunkang <sanghunkang.dev@gmail.com>
2019-09-10 00:24:34 +0900

    registered project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aascraw",
+    version="0.2",
+    author="Sanghun Kang",
+    author_email="sanghunkang.dev@gmail.com",
+    description="An ambitious attempt to automatically build web scrapers/crawlers",
+
+    packages=find_packages()
+)
